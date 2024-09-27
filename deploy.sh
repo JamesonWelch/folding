1c85c69a2276df79810ee1fcf7c37d605688ebeb
JamesonWelch <jamesonb.welch@gmail.com>
2024-09-27 10:28:43 -0700

    Initial config

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ git add .
 git commit -m "sync"
 git push
 
-scp serveradmin@192.168.0.18:
+scp .env serveradmin@192.168.0.18:/home/serveradmin/folding
 
 _ssh fold "cd folding && git pull && docker-compose up -d --force-recreate
