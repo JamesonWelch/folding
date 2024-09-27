5ace54d56f957074dc56b3525053e9f3acd00e26
JamesonWelch <jamesonb.welch@gmail.com>
2024-09-27 10:29:06 -0700

    sync

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git push
 
 scp .env serveradmin@192.168.0.18:/home/serveradmin/folding
 
-_ssh fold "cd folding && git pull && docker-compose up -d --force-recreate
+_ssh fold "cd folding && git pull && docker-compose up -d --force-recreate"
